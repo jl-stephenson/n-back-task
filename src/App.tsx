b03badedb2b62ab0f66f0e5cc7b89ba8dac39385
John <johnlouisstephenson@gmail.com>
2025-04-25 15:42:13 +0100

    feat: minimal animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
-  return <></>;
+  const [displayLetter, setDisplayLetter] = useState("");
+  const [index, setIndex] = useState(0);
+
+  const array = ["A", "B", "C", "D", "E", "F"];
+
+  useEffect(() => {
+    if (index >= array.length) return;
+
+    setDisplayLetter(array[index]);
+
+    const hideInterval = setInterval(() => {
+      setDisplayLetter("");
+    }, 500);
+
+    const nextLetterInterval = setInterval(() => {
+      setIndex((index) => index + 1);
+    }, 3000);
+
+    return () => {
+      clearInterval(hideInterval);
+      clearInterval(nextLetterInterval)};
+  }, [array.length, index]);
+
+  return (
+    <>
+      <div>
+        <h1>{displayLetter}</h1>
+      </div>
+    </>
+  );
 }

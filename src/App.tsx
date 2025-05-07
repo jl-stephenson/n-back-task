e158fe66db5fa39d8c007be2ae4b259b069ce0e9
John <johnlouisstephenson@gmail.com>
2025-05-07 15:07:47 +0100

    feature: results displayed

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,47 @@ type Trial = {
   id: number;
   timeStarted: number;
   correctCount: number;
+  falseAlarmCount: number;
   missCount: number;
 };
 
 export default function App() {
   const [displayLetter, setDisplayLetter] = useState("");
   const [index, setIndex] = useState(0);
-  const currentLetterRef = useRef("");
-  const isMatchRef = useRef(false);
+  const [isKeydown, setIsKeydown] = useState(false);
+  const [isEnd, setIsEnd] = useState(false);
   const [trial, setTrial] = useState<Trial>({
     id: Date.now(),
     timeStarted: Date.now(),
     correctCount: 0,
+    falseAlarmCount: 0,
     missCount: 0,
   });
 
+  const isMatchRef = useRef(false);
+  const currentLetterRef = useRef("");
+
   useEffect(() => {
-    if (index >= LETTERS.length) return;
+    if (isEnd) return;
+    if (!isKeydown && isMatchRef.current) {
+      setTrial((prevTrial) => {
+        const newCount = prevTrial.missCount + 1;
+        return {
+          ...prevTrial,
+          missCount: newCount,
+        };
+      });
+    }
+
+    setIsKeydown(false);
+
+    if (
+      index >= LETTERS.length ||
+      trial.falseAlarmCount + trial.missCount >= 2
+    ) {
+      setIsEnd(true);
+      return;
+    }
 
     currentLetterRef.current = LETTERS[index];
 
@@ -42,13 +66,15 @@ export default function App() {
       clearTimeout(hideTimeout);
       clearTimeout(nextLetterTimeout);
     };
-  }, [index]);
+  }, [index, isKeydown, trial.falseAlarmCount, trial.missCount, isEnd]);
 
   useEffect(() => {
     function handleKeyPress(event: KeyboardEvent) {
+      if (isEnd) return;
+      setIsKeydown(true);
       if (event.key.toLowerCase() === "m") {
         setTrial((prevTrial) => {
-          const field = isMatchRef.current ? "correctCount" : "missCount";
+          const field = isMatchRef.current ? "correctCount" : "falseAlarmCount";
           const newCount = prevTrial[field] + 1;
           return {
             ...prevTrial,
@@ -66,10 +92,11 @@ export default function App() {
     <>
       <div>
         <h1>{displayLetter}</h1>
-        {index >= LETTERS.length && (
+        {isEnd && (
           <div>
             <h2>Results</h2>
             <p>Total Correct: {trial.correctCount}</p>
+            <p>Total False Alarms: {trial.falseAlarmCount}</p>
             <p>Total Misses: {trial.missCount}</p>
           </div>
         )}

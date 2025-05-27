524697894be64a82a4006f985e1e2c2309fdbfe0
John <johnlouisstephenson@gmail.com>
2025-05-27 12:26:20 +0100

    test: add clock for mocking interval

--- tests/happy-path.spec.ts
+++ tests/happy-path.spec.ts
@@ -7,14 +7,20 @@ test("happy path to results", async ({ page }) => {
   await page.getByRole("button", { name: /continue/i }).click();
   await expect(page).toHaveURL(/\/intro$/);
 
+  await page.clock.install({ time: 0 });
+
   await page.getByRole("button", { name: /start game/i }).click();
   await expect(page).toHaveURL(/\/task$/);
 
-  await expect(page.getByTestId("display-letter")).toHaveText(/^[A-Z]$/);
+  await expect(page.getByTestId("display-letter")).toHaveText("A");
+  await page.clock.runFor(200);
+  await expect(page.getByTestId("display-letter")).toHaveText("A");
+  await page.keyboard.press("m");
+  await expect(page.getByText("Correctly identified match")).toBeVisible();
 
   await expect(page).toHaveURL(/\/results$/);
 
-  await expect(page.getByText("Total Correct: 0")).toBeVisible();
+  await expect(page.getByText("Total Correct: 1")).toBeVisible();
   await expect(page.getByText("Total False Alarms: 0")).toBeVisible();
   await expect(page.getByText("Total Misses: 2")).toBeVisible();
 });
